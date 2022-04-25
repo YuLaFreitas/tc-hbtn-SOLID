5dc4ce18ed4e3e63e30735f8387b6aa29e837316
yfreitasCIET <yfreitas@ciandt.com>
2022-04-25 14:59:21 -0300

    Classe CustomerService.java SOLID

---
+++ srp/CustomerService.java
@@ -0,0 +1,29 @@
+public class CustomerService {
+
+    public void ListCustomer(){
+    }
+
+    public void addCustomer(){
+    }
+
+    public void updateCustomer(){
+    }
+
+    public void deleteCustomer(){
+    }
+
+    public void notifyCustomer(){
+    }
+
+    public void sendSMS(){
+
+    }
+
+    public void sendEmail(){
+
+    }
+
+    public void evaluationCustomer(){
+    }
+
+}
