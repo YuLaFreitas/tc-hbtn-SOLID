65f6c2259ca83ca5add3ea52ea314c7ef83a8197
yfreitasCIET <yfreitas@ciandt.com>
2022-04-25 14:56:02 -0300

    SOLID

---
+++ srp/CustomerEvaluation.java
@@ -0,0 +1,16 @@
+public class CustomerEvaluation {
+    public void ListCustomer(){
+    }
+
+    public void addCustomer(){
+    }
+
+    public void updateCustomer(){
+    }
+
+    public void deleteCustomer(){
+    }
+
+    public void evaluationCustomer(){
+    }
+}
---
+++ srp/NotificationService.java
@@ -0,0 +1,14 @@
+public class NotificationService {
+
+    public void notifyCustomer(){
+    }
+
+    public void sendSMS(){
+
+    }
+
+    public void sendEmail(){
+
+    }
+
+}
