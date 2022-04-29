c61a4b07d3c2f31b439e5225fe33dc543bc4d373
yfreitasCIET <yfreitas@ciandt.com>
2022-04-29 16:20:50 -0300

    Correção do log

---
+++ 0x04/log/LoggerInFile.java
@@ -0,0 +1,36 @@
+package log;
+
+import java.io.IOException;
+
+import java.util.logging.FileHandler;
+
+import java.util.logging.Logger;
+
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInFile {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger("MyLog");
+
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("logs.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.addHandler(fileHandler);
+
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+
+        fileHandler.setFormatter(simpleFormatter);
+
+        logger.info("Log test");
+
+        logger.info("Hi In the main class test");
+
+    }
+
+}
